1f06612556b72a4c74946c7b13e67806d3c93aac
Steve Kuo <steve.kuo@makerstudios.com>
2013-03-22 11:53:02 -0700

    Config request log in zrest-jetty.xml

--- src/main/bin/startZRest.sh
+++ src/main/bin/startZRest.sh
@@ -2,4 +2,4 @@ JVM_MEM=1700M
 
 echo "Starting zrest-Web (MEM = -server -Xms$JVM_MEM -Xmx$JVM_MEM)"
 
-$JAVA_HOME/bin/java -server -Xms$JVM_MEM -Xmx$JVM_MEM -jar lib/jetty-runner-${jetty.version}.jar --stop-port 8181 --stop-key zrestStop --path /zrest-web zrest-web-${project.version}.war > /dev/null 2>&1 &
+$JAVA_HOME/bin/java -server -Xms$JVM_MEM -Xmx$JVM_MEM -jar lib/jetty-runner-${jetty.version}.jar --config conf/zrest-jetty.xml --stop-port 8181 --stop-key zrestStop --path /zrest-web zrest-web-${project.version}.war > console.log 2>&1 &
