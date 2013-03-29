7e57a5d8e0c076bd92ea6e9818b2f1b89f980220
Steve Kuo <steve.kuo@makerstudios.com>
2013-03-29 08:04:46 -0700

    Add a method for mixed path variable and request params.

--- src/main/java/org/zrest/rest/BidController.java
+++ src/main/java/org/zrest/rest/BidController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.zrest.db.BidDao;
 import org.zrest.model.Bid;
@@ -76,4 +77,31 @@ public class BidController {
         return bidStatus;
     }
 
+    @RequestMapping(method = RequestMethod.GET, value="/bids/{sourceId}", headers = "accept=application/json")
+    @ResponseBody
+    /**
+     * Return bid in json format and status code of 200, 404 and 500.
+     * @param sourceId original source id
+     * @param source source identifier
+     * @param id optional request param, not used.
+     * @param response HttpServletResponse
+     * @return BidStatus in json format
+     * @throws SQLException returns 500 status with error html page
+     * @throws IOException returns 500 status with error html page
+     */
+    public Bid getBidByPathAndParams(@PathVariable String sourceId, @RequestParam("source") String source,
+            @RequestParam(value = "id", required = false) Long id, HttpServletResponse response) throws IOException {
+        log.info("getBidByPathAndParams: sourceId=" + sourceId + ", source=" + source + ", id=" + id);
+        Bid bid = null;
+        try {
+            bid = bidDao.get(sourceId, source);
+            if (bid == null)
+                // use setStatus() instead of sendError() because it allows
+                // ResponseBody to be written
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (SQLException sqle) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        return bid;        
+    }
 }
